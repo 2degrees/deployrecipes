01280d54156d68df9194bc85edcdcaf3dd7d79c0
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2013-06-07 14:10:19 +0100

    Exposed method ``get_config_variables`` on recipe to allow for sub-classing

--- deployrecipes.py
+++ deployrecipes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 #
-# Copyright (c) 2010-2011, 2degrees Limited.
+# Copyright (c) 2010-2011, 2013, 2degrees Limited.
 # All Rights Reserved.
 #
 # This file is part of deployrecipes:
@@ -34,37 +34,48 @@ class ConfvarsRecipe(object):
     """
     
     def __init__(self, buildout, name, options):
-        if "config_uri" not in options:
-            raise BuildoutError('PasteDeploy config URI not set in the '
-                                '"config_uri" option')
+        if 'config_uri' not in options:
+            raise BuildoutError(
+                'PasteDeploy config URI not set in the "config_uri" option',
+                )
         
         # Loading the distribution:
-        distribution = options.pop("factory_distribution", None)
+        distribution = options.pop('factory_distribution', None)
         if not distribution:
-            raise BuildoutError('"factory_distribution" option not set in [%s] '
-                                'to the package containing the PasteDeploy '
-                                'application factory' % name)
-        if "eggs" not in options:
+            raise BuildoutError(
+                '"factory_distribution" option not set in [%s] to the package '
+                    'containing the PasteDeploy application factory' % name,
+                )
+        if 'eggs' not in options:
             # If we don't set the "eggs" option, it will take the part name:
-            options['eggs'] = ""
+            options['eggs'] = ''
         ws = Eggs(buildout, name, options).working_set([distribution])[1]
         for dist in ws:
             working_set.add(dist)
         
         # Loading the variables in the PasteDeploy config:
         buildout_dir = buildout['buildout']['directory']
-        try:
-            config_variables = appconfig(options['config_uri'], 
-                                         relative_to=buildout_dir)
-        except (LookupError, ValueError, OSError), exc:
-            raise BuildoutError("Could not load variables from part '%s': %s"
-                                % (name, exc))
-        else:
-            options.update(config_variables)
+        config_variables = self.get_config_variables(
+            options['config_uri'],
+            buildout_dir,
+            name,
+            )
+        options.update(config_variables)
     
     def install(self):
         return ()
     
     def update(self):
         pass
-
+    
+    def get_config_variables(self, config_uri, buildout_dir, part_name):
+        try:
+            config_variables = appconfig(config_uri, relative_to=buildout_dir)
+        except (LookupError, ValueError, OSError), exc:
+            raise BuildoutError(
+                "Could not load variables from part '%s': %s" % (
+                    part_name,
+                    exc,
+                    ),
+                )
+        return config_variables
